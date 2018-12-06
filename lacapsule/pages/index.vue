f89ce59fefde12d1bacbe0a4630daa984864bc41
ElisabethStereosuper <elisabeth@stereosuper.fr>
2018-12-06 19:47:27 +0100

    :art: home + btn

--- lacapsule/pages/index.vue
+++ lacapsule/pages/index.vue
@@ -8,17 +8,96 @@
                 <div v-html='homeText'/>
             </div>
         </div>
+
+        <div class='earth'></div>
+        <div class='ovni'></div>
     </div>
 </template>
 
 <script>
 import Prismic from 'prismic-javascript';
-import SiteHeader from '~/components/Header.vue';
 import PrismicDOM from 'prismic-dom';
+
+import { TweenMax } from 'gsap';
+
+import SiteHeader from '~/components/Header.vue';
 
 export default {
     components: {
         SiteHeader
+    },
+    mounted: function() {
+        const btn = document.getElementsByClassName('button');
+        let glow,
+            i = 0,
+            nbBtn = btn.length,
+            btnCoordinates,
+            mouseX,
+            mouseY,
+            glowX,
+            glowY,
+            distanceMax,
+            distance;
+
+        for (i; i < nbBtn; i++) {
+            glow = document.createElement('i');
+            glow.className = 'glow';
+
+            btn[i].querySelector('a').appendChild(glow);
+
+            btn[i].addEventListener('mouseenter', function(e) {
+                TweenMax.to(glow, 0.2, { opacity: 1 });
+
+                btnCoordinates = this.getBoundingClientRect();
+                mouseX = this.offsetWidth - (e.clientX - btnCoordinates.x); // inverted position x in btn
+                mouseY = this.offsetHeight - (e.clientY - btnCoordinates.y); // inverted position y in btn
+
+                if (mouseX <= 0) {
+                    glowX = 0;
+                } else if (mouseX >= this.offsetWidth) {
+                    glowX = this.offsetWidth;
+                } else {
+                    glowX = mouseX;
+                }
+
+                if (mouseY <= 0) {
+                    glowY = 0;
+                } else if (mouseY >= this.offsetHeight) {
+                    glowY = this.offsetHeight;
+                } else {
+                    glowY = mouseY;
+                }
+
+                distance = 1 - Math.abs(e.clientX - btnCoordinates.x - glowX)/this.offsetWidth;
+
+                TweenMax.set(glow, { x: glowX, y: glowY, scale: distance });
+            });
+
+            btn[i].addEventListener('mousemove', function(e) {
+                mouseX = this.offsetWidth - (e.clientX - btnCoordinates.x); // inverted position x in btn
+                mouseY = this.offsetHeight - (e.clientY - btnCoordinates.y); // inverted position y in btn
+
+                if (mouseX <= 0) {
+                    glowX = 0;
+                } else if (mouseX >= this.offsetWidth) {
+                    glowX = this.offsetWidth;
+                } else {
+                    glowX = mouseX;
+                }
+
+                if (mouseY <= 0) {
+                    glowY = 0;
+                } else if (mouseY >= this.offsetHeight) {
+                    glowY = this.offsetHeight;
+                } else {
+                    glowY = mouseY;
+                }
+
+                distance = 1 - Math.abs(e.clientX - btnCoordinates.x - glowX)/this.offsetWidth;
+
+                TweenMax.set(glow, { x: glowX, y: glowY, scale: distance });
+            });
+        }
     },
     async asyncData() {
         const apiEndpoint = 'https://lacapsule.cdn.prismic.io/api/v2';
@@ -47,10 +126,28 @@ h1 {
 .container {
     position: relative;
     top: -50%;
-    margin-top: 30vh;
+    margin-top: 35vh;
+    z-index: 1;
 }
 
 .text {
     width: 40%;
+}
+
+.earth {
+    width: 358px;
+    height: 357px;
+    position: absolute;
+    top: 0;
+    left: 25%;
+    background: url(../static/img/earth.png) no-repeat 0 0 / 100%;
+}
+.ovni {
+    width: 1038px;
+    height: 677px;
+    position: absolute;
+    top: 25%;
+    left: 54%;
+    background: url(../static/img/ovni.png) no-repeat 0 0 / 100%;
 }
 </style>
