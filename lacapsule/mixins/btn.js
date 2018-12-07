aa8d4d2dafabd0b0b04dfe1b71373bd4dd7b48f6
ElisabethStereosuper <elisabeth@stereosuper.fr>
2018-12-07 18:52:20 +0100

    :rocket: clean btn.js

--- lacapsule/mixins/btn.js
+++ lacapsule/mixins/btn.js
@@ -8,8 +8,6 @@ export default {
             i = 0,
             nbBtn = btn.length,
             btnCoordinates,
-            mouseX,
-            mouseY,
             glowX,
             glowY,
             distance,
@@ -38,31 +36,15 @@ export default {
 
             btn[i].addEventListener('mousemove', function(e) {
                 // inverted cursor position in btn
-                mouseX = btnW - (e.clientX - btnCoordinates.x);
-                mouseY = btnH - (e.clientY - btnCoordinates.y);
-
-                if (mouseX <= 0) {
-                    glowX = 0;
-                } else if (mouseX >= btnW) {
-                    glowX = btnW;
-                } else {
-                    glowX = mouseX;
-                }
-
-                if (mouseY <= 0) {
-                    glowY = 0;
-                } else if (mouseY >= btnH) {
-                    glowY = btnH;
-                } else {
-                    glowY = mouseY;
-                }
+                glowX = btnW - (e.clientX - btnCoordinates.x);
+                glowY = btnH - (e.clientY - btnCoordinates.y);
 
                 // distance from glow to cursor
-                distance = 1.3 - Math.abs(e.clientX - btnCoordinates.x - glowX)/btnW;
+                distance = 1.3 - Math.abs(e.clientX - btnCoordinates.x - glowX) / btnW;
 
                 TweenMax.set(glow, { x: glowX, y: glowY, scale: distance });
 
-                if(!TweenMax.isTweening(glow)){
+                if (!TweenMax.isTweening(glow)) {
                     TweenMax.set(glow, { opacity: distance });
                 }
             });
