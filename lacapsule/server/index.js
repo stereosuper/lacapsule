8db9a227280d8d4d3c0e9a9b065cf6f65da2a003
Adrien Le Menthéour <adrienlementheour@macbook-pro-de-adrien.home>
2018-12-21 14:01:15 +0100

    👷‍♂️ env alwaysdata

--- lacapsule/server/index.js
+++ lacapsule/server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const consola = require('consola');
-const { Nuxt, Builder } = require('nuxt');
+const {
+  Nuxt,
+  Builder
+} = require('nuxt');
 const app = express();
-const host = process.env.HOST || '127.0.0.1';
-const port = process.env.PORT || 3000;
+const host = process.env.ALWAYSDATA_HTTPD_IP || '127.0.0.1';
+const port = process.env.ALWAYSDATA_HTTPD_PORT || 3000;
 
 app.set('port', port);
 
@@ -12,23 +15,23 @@ let config = require('../nuxt.config.js');
 config.dev = !(process.env.NODE_ENV === 'production');
 
 async function start() {
-    // Init Nuxt.js
-    const nuxt = new Nuxt(config);
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config);
 
-    // Build only in dev mode
-    if (config.dev) {
-        const builder = new Builder(nuxt);
-        await builder.build();
-    }
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    await builder.build();
+  }
 
-    // Give nuxt middleware to express
-    app.use(nuxt.render);
+  // Give nuxt middleware to express
+  app.use(nuxt.render);
 
-    // Listen the server
-    app.listen(port, host);
-    consola.ready({
-        message: `Server listening on http://${host}:${port}`,
-        badge: true
-    });
+  // Listen the server
+  app.listen(port, host);
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  });
 }
 start();
