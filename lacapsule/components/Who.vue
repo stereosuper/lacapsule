aad53774d08943ca03677c8e80d5669caddfee95
ElisabethStereosuper <elisabeth@stereosuper.fr>
2019-09-02 11:06:03 +0200

    :art: linkedin

--- lacapsule/components/Who.vue
+++ lacapsule/components/Who.vue
@@ -13,6 +13,11 @@
                     <li v-for='member in team' :key='member.name'>
                         <img v-if='member.photo.url' :src='member.photo.url' :alt='member.photo.alt' :whidth='member.photo.dimensions.width' :height='member.photo.dimensions.height'>
                         <p>{{member.name}}</p>
+                        <a v-if='member.linkedin.url' :href='member.linkedin.url' :target='member.linkedin.target'>
+                            <svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill='currentColor' fill-rule="evenodd" clip-rule="evenodd" d="M14.1123 27C21.5079 27 27.5033 21.0046 27.5033 13.6089C27.5033 6.21325 21.5079 0.217865 14.1123 0.217865C6.71657 0.217865 0.721191 6.21325 0.721191 13.6089C0.721191 21.0046 6.71657 27 14.1123 27ZM10.0719 9.85636C9.36368 9.85636 8.79248 9.28316 8.79248 8.57764C8.79248 7.87212 9.36368 7.29892 10.0719 7.29892C10.7767 7.29892 11.3493 7.87212 11.3493 8.57764C11.3493 9.28316 10.7767 9.85636 10.0719 9.85636ZM19.4321 17.9189H17.2277V14.4691C17.2277 13.6466 17.2131 12.5886 16.082 12.5886C14.9349 12.5886 14.7601 13.485 14.7601 14.4106V17.9189H12.5577V10.8251H14.6709V11.7953H14.7015C14.9954 11.2374 15.7149 10.6489 16.7875 10.6489C19.0198 10.6489 19.4321 12.1178 19.4321 14.0283V17.9189ZM11.1741 17.919H8.96706V10.8252H11.1741V17.919Z"/>
+                            </svg>
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -143,6 +148,15 @@ export default {
         }
         &:nth-child(2){
             margin-top: 50px;
+        }
+    }
+    a{
+        position: absolute;
+        bottom: 5px;
+        right: 20px;
+        color: #fff;
+        &:hover, &:focus{
+            color: $primary;
         }
     }
     p{
