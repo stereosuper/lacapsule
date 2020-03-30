a6a0f781cc4a4fbb285c9b72b3d93f582c237639
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-03-30 12:55:20 +0200

    🐛 fix ref click

--- lacapsule/components/ListRef.vue
+++ lacapsule/components/ListRef.vue
@@ -1,26 +1,26 @@
 <template>
-    <li>
-        <div v-if='!reference.contact'>
-            <div class='logo' v-if='reference.logo.url'>
-                <img :src='reference.logo.url' :alt='reference.logo.alt'/>
-            </div>
-            <h2>{{reference.company}}</h2>
-            <p>{{reference.title}}</p>
-            <div class='button'>
-                <nuxt-link :to='reference.url'>Plus de détails</nuxt-link>
-            </div>
-        </div>
+  <li>
+    <div v-if="!reference.contact">
+      <div v-if="reference.logo.url" class="logo">
+        <img :src="reference.logo.url" :alt="reference.logo.alt">
+      </div>
+      <h2>{{ reference.company }}</h2>
+      <p>{{ reference.title }}</p>
+      <div class="button">
+        <nuxt-link :to="reference.url">Plus de détails</nuxt-link>
+      </div>
+    </div>
 
-        <div v-if='reference.contact' class='contact'>
-            <div class='logo'>
-                <img src='/img/helmet-contact.svg' alt=''>
-            </div>
-            <h2>Et pourquoi pas vous ?</h2>
-            <div class='button'>
-                <nuxt-link to='/contact'>Contactez-nous</nuxt-link>
-            </div>
-        </div>
-    </li>
+    <div v-if="reference.contact" class="contact">
+      <div class="logo">
+        <img src="/img/helmet-contact.svg" alt="">
+      </div>
+      <h2>Et pourquoi pas vous ?</h2>
+      <div class="button">
+        <nuxt-link to="/contact">Contactez-nous</nuxt-link>
+      </div>
+    </div>
+  </li>
 </template>
 
 <script>
@@ -35,18 +35,18 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-@import "./assets/scss/abstracts/_variables.scss";
+@import './assets/scss/abstracts/_variables.scss';
 
-li{
+li {
     width: calc(25% - 15px);
     padding: 90px 30px 25px;
     margin: 0 7px;
     text-align: center;
     z-index: 1;
-    &:before{
+    &:before {
         content: none;
     }
-    &:after{
+    &:after {
         content: '';
         height: 130%;
         position: absolute;
@@ -56,12 +56,13 @@ li{
         z-index: -1;
         border-radius: 18px;
         background: linear-gradient(360deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0) 71.6%);
+        pointer-events: none;
     }
-    &:nth-child(2n){
+    &:nth-child(2n) {
         margin-top: 50px;
         margin-bottom: -50px;
     }
-    > div{
+    > div {
         display: flex;
         height: 100%;
         flex-wrap: wrap;
@@ -69,21 +70,21 @@ li{
     }
 }
 
-h2{
+h2 {
     width: 100%;
     font-size: 2.4rem;
     margin: 1em 0 0;
-    .contact &{
+    .contact & {
         max-width: 190px;
         margin: 1em auto;
     }
 }
 
-p{
+p {
     width: 100%;
 }
 
-.logo{
+.logo {
     display: flex;
     width: 100%;
     max-width: 250px;
@@ -95,15 +96,15 @@ p{
     z-index: 1;
     background: rgba(0, 0, 0, 0.25);
     background: #fff;
-    &:before{
+    &:before {
         content: '';
         display: block;
         padding: 0 0 100%;
         z-index: -2;
     }
-    .contact &{
+    .contact & {
         background: rgba(#000, 0.1);
-        &:after{
+        &:after {
             content: '';
             position: absolute;
             top: 20px;
@@ -115,7 +116,7 @@ p{
             z-index: -1;
         }
     }
-    img{
+    img {
         position: absolute;
         top: 0;
         right: 0;
@@ -125,47 +126,46 @@ p{
     }
 }
 
-img{
+img {
     max-width: 80%;
 }
 
-.button{
+.button {
     width: 100%;
     align-self: flex-end;
 }
 
-
-@media (max-width: $container){
-    li{
+@media (max-width: $container) {
+    li {
         padding: 90px 15px 15px;
-        &:nth-child(2n){
+        &:nth-child(2n) {
             margin-top: 0;
             margin-bottom: 0;
         }
     }
 }
 
-@media (max-width: $desktop){
-    li{
+@media (max-width: $desktop) {
+    li {
         width: calc(33% - 14px);
         margin: 0 7px;
         padding: 90px 20px 20px;
     }
 }
 
-@media (max-width: $tablet){
-    li{
+@media (max-width: $tablet) {
+    li {
         width: calc(50% - 20px);
         margin: 0 10px 50px;
         padding-top: 50px;
-        &:nth-child(2n){
+        &:nth-child(2n) {
             margin-bottom: 50px;
         }
     }
 }
 
-@media (max-width: $phone){
-    li{
+@media (max-width: $phone) {
+    li {
         width: 100%;
     }
 }
